9a87477a3189fd915f9c059bd25f8f59c164a4a7
Nhung <hongnhung16052003@gmail.com>
2025-04-23 01:58:39 +0700

    feat: apply forget password API

--- pages/forget-password.vue
+++ pages/forget-password.vue
@@ -1,26 +1,41 @@
 <template>
-<div class="flex justify-center items-center h-[100vh]">
-    <div class="flex-1 p-[20px] md:p-[30px] lg:py-[20px] lg:px-[60px]">
-        <h1 class="text-[rgba(6,80,118,0.9)] font-bold text-[24px] text-center">{{ t('auth.forget_password').toUpperCase() }}</h1>
-        <div class="mt-[20px]">
-            <BaseInput v-model="forgetData.email" :error="forgetErrors.email" :label="t('auth.forget_password_message')" cursorIcon="pointer" :placeholder="t('auth.email_placeholder')" @blur="handleBlurInput('email')"/>
+    <div class="flex justify-center items-center">
+        <div class="flex-1 p-[20px] md:p-[30px] lg:py-[20px] lg:px-[60px]">
+            <div>
+                <h1 class="text-[rgba(6,80,118,0.9)] font-bold text-[24px] text-center">{{ t('auth.forget_password').toUpperCase() }}</h1>
+            <div class="mt-[20px]">
+                <BaseInput v-model="forgetData.email" :error="forgetErrors.email" :label="t('auth.forget_password_message')" cursorIcon="pointer" :placeholder="t('auth.email_placeholder')" @blur="handleBlurInput('email')"/>
+            </div>
+           <div class="mt-[30px]">
+            <BaseButton :text="t('auth.confirm')" variant="primary" height="40px" width="100%" :disabled="isDisabled" @click="handleForgetPassword"/>
+            </div>
+
+            </div>
+            <div class="flex flex-col justify-center mt-[30px]">
+                <div class="mt-[16px] text-center">
+                <span class="text-[14px]">{{ t('auth.already_have_account') }}</span>
+                <NuxtLink class="text-[rgba(6,80,118,0.9)] text-[14px] ml-[5px]" :to="ROUTES.LOGIN">{{ t('auth.login') }}</NuxtLink>
+            </div>
+        <div class="option-text flex items-center gap-[10px] mt-[24px] text-[13px]">{{ t('auth.or') }}</div>
+      <div class="mt-[16px]"> <BaseButton :text="t('auth.login_with_google')" variant="outline" radius="0" leftIcon="google" sizeIcon="24px" height="38px"></BaseButton></div>
+            </div>
         </div>
-       <div class="mt-[30px]">
-        <BaseButton :text="t('auth.confirm')" variant="primary" height="40px" width="100%" :disabled="isDisabled"/>
+        <div class="hidden sm:block h-[100vh] flex-1" >
+            <img class="w-[100%] h-[100%] object-fit-contain" src="/images/login.png" alt="sign-up"/>
         </div>
     </div>
-    <div class="hidden sm:block h-[100vh] flex-1" >
-        <img class="w-[100%] h-[100%] object-fit-contain" src="/images/login.png" alt="sign-up"/>
-    </div>
-</div>
 </template>
 <script setup>
 import BaseButton from '~/components/BaseButton.vue';
 import BaseInput from '~/components/BaseInput.vue';
 import { getForgetPassSchema } from '~/schemas/forgetPassSchema';
+import { authService } from '~/service-api/authService';
+import { ROUTES } from '~/constants/routes';
 
 const { t } = useTranslation()
 const forgetPassSchema = getForgetPassSchema(t)
+const toast = useToast()
+const router = useRouter()
 
 const forgetData = ref({
   email: ''
@@ -31,7 +46,7 @@ const forgetErrors = ref({
 })
 
 const isDisabled = computed(() => {
-  return !forgetData.value.email || forgetErrors.value.email
+  return !forgetData.value.email || !!forgetErrors.value.email
 })
 
 const handleBlurInput = (field) => {
@@ -50,7 +65,35 @@ const handleBlurInput = (field) => {
     }
 }
 
-
-
-
+const handleForgetPassword = async () => {
+    try {
+        await authService.forgetPassword(forgetData.value.email)
+        toast.add({ severity: 'success', summary: 'Success', detail: 'Check your email to reset password', life: 3000 })
+        router.push(ROUTES.LOGIN)
+    }
+    catch (error) {
+         if (error?.response) {
+       toast.add({ severity: 'error', summary: 'Error Login', detail: getMessageError(error), life: 3000 });
+      }
+      else {
+       toast.add({ severity: 'error', summary: 'Error Login', detail: error.message, life: 3000 })
+    }
+    }
+}
 </script>
+<style scoped>
+.option-text::before {
+    content: '';
+    display: inline-block;
+    width: 100%;
+    height: 1px;
+    background-color: #00000033;
+}
+.option-text::after {
+    content: '';
+    display: inline-block;
+    width: 100%;
+    height: 1px;
+    background-color: #00000033;
+}
+</style>
