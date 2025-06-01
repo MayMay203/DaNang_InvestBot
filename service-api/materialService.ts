a19fc8e76a4044670049e200a59193f12c7fe4c4
Nhunglth <hongnhung16052003@gmail.com>
2025-06-01 14:12:34 +0700

    feat: handle save user's material

--- service-api/materialService.ts
+++ service-api/materialService.ts
@@ -1,42 +1,65 @@
 import { API_ENPOINT } from "~/constants/api-endpoints"
 import type { IChangeStatus, Material } from "~/models/IMaterial";
 
-class MaterialService{
-    getAllMaterials(store="empty") {
-        const { $axiosApi } = useNuxtApp()
-        const url = store ? `${API_ENPOINT.GET_ALL_MATERIALS}?store=${store}` : API_ENPOINT.GET_ALL_MATERIALS
-        return $axiosApi.get(url)
+class MaterialService {
+  getAllMaterials(store = "empty", role?: string) {
+    const { $axiosApi } = useNuxtApp();
+
+    const queryParams = new URLSearchParams();
+    queryParams.append("store", store);
+
+    if (role) {
+      queryParams.append("role", role);
     }
 
-    async uploadMaterial(formData:any) {
-        const { $axiosApi } = useNuxtApp();
-        $axiosApi.post(API_ENPOINT.UPLOAD_MATERIAL, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-    }
+    const url = `${API_ENPOINT.GET_ALL_MATERIALS}?${queryParams.toString()}`;
+    return $axiosApi.get(url);
+  }
 
-    getDetailMaterial(id:number) {
-        const { $axiosApi } = useNuxtApp()
-        return $axiosApi.get(`${API_ENPOINT.DETAIL_MATERIAL}/${id}`);
-    }
+  async uploadMaterial(formData: any) {
+    const { $axiosApi } = useNuxtApp();
+    $axiosApi.post(API_ENPOINT.UPLOAD_MATERIAL, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
 
-    updateMaterial(materialData:Material) {
-        const {id, name, description, text, url} = materialData
-        const { $axiosApi } = useNuxtApp();
-        return $axiosApi.post(`${API_ENPOINT.UPDATE_MATERIAL}/${id}`, {name, description, text, url});
-    }
+  getDetailMaterial(id: number) {
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.get(`${API_ENPOINT.DETAIL_MATERIAL}/${id}`);
+  }
 
-    changeStatusMaterial(data: IChangeStatus) {
-        const { $axiosApi } = useNuxtApp();
-        return $axiosApi.patch(API_ENPOINT.CHANGE_STATUS_MATERIAL, { ...data });
-    }
+  updateMaterial(materialData: Material) {
+    const { id, name, description, text, url } = materialData;
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.post(`${API_ENPOINT.UPDATE_MATERIAL}/${id}`, {
+      name,
+      description,
+      text,
+      url,
+    });
+  }
 
-    deleteMaterial(id: number) {
-        const { $axiosApi } = useNuxtApp();
-        return $axiosApi.delete(`${API_ENPOINT.DELETE_MATERIAL}/${id}`);
-    }
+  changeStatusMaterial(data: IChangeStatus) {
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.patch(API_ENPOINT.CHANGE_STATUS_MATERIAL, { ...data });
+  }
+
+  deleteMaterial(id: number) {
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.delete(`${API_ENPOINT.DELETE_MATERIAL}/${id}`);
+  }
+
+  deleteUserMaterial(id: number) {
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.delete(`${API_ENPOINT.DELETE_USER_MATERIAL}/${id}`);
+  }
+
+  syncMaterial(id: number) {
+    const { $axiosApi } = useNuxtApp();
+    return $axiosApi.post(`${API_ENPOINT.SYNC_MATERIAL}/${id}`);
+  }
 }
 
 export const materialService = new MaterialService()
